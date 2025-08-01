7bba31ae924307f9a339a03cfbe3462edeb8eea4
Shawon Ashraf <shawon.ashraf@proton.me>
2025-08-01 19:01:16 +0600

    reformat

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-mod emoji_generator;
 mod clipboard;
+mod emoji_generator;
 
 fn main() {
     let generator = emoji_generator::EmojiGenerator::new();
     let unique_emojis = generator.generate(5);
-    
+
     // convert the emojis to a single string
     let result = unique_emojis.join(" ");
-    
+
     // copy to clipboard
     println!("Copying to clipboard: {}", result);
-    
+
     match clipboard::copy_to_clipboard(&result) {
         Ok(_) => println!("Emojis copied to clipboard successfully!"),
         Err(e) => eprintln!("Error copying to clipboard: {}", e),
