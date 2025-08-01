10d67bc064b31016bb03adc7ce55535daec8b539
Shawon Ashraf <shawon.ashraf@proton.me>
2025-08-01 09:33:43 +0600

    runner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod emoji_generator;
+
 fn main() {
-    println!("Hello, world!");
+    let generator = emoji_generator::EmojiGenerator::new();
+    let unique_emojis = generator.generate(5);
+    
+    // convert the emojis to a single string
+    let result = unique_emojis.join(" ");
+    println!("{:}", result);
 }
