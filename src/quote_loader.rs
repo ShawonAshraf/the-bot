239eb1e8e6f25f5bdf5d5f3f90485a7aa1c3d985
Shawon Ashraf <shawon.ashraf@proton.me>
2025-08-12 16:26:30 +0600

    load from folder

--- src/quote_loader.rs
+++ src/quote_loader.rs
@@ -3,26 +3,37 @@ use rfortune::loader;
 pub fn load_from_file(file_path: &str) -> Result<Vec<String>, std::io::Error> {
    let path = std::path::Path::new(file_path);
    let quotes = loader::FortuneFile::from_file(path);
-    
+
     if let Ok(quotes) = quotes {
-        return Ok(quotes.quotes);
-    } else { 
-        return Err(std::io::Error::new(
+        Ok(quotes.quotes)
+    } else {
+        Err(std::io::Error::new(
             std::io::ErrorKind::NotFound,
             format!("Failed to load fortunes from file: {:?}", path),
-        ));
+        ))
     }
 }
 
-pub fn load_all(file_paths: &[&str]) -> Result<Vec<String>, std::io::Error> {
+
+pub fn load_from_folder(folder_path: &str) -> Result<Vec<String>, std::io::Error> {
     let mut all_quotes = Vec::new();
+    let path = std::path::Path::new(folder_path);
     
-    for file_path in file_paths {
-        match load_from_file(file_path) {
-            Ok(quotes) => all_quotes.extend(quotes),
-            Err(e) => return Err(e),
+    if path.is_dir() {
+        for entry in std::fs::read_dir(path)? {
+            let entry = entry?;
+            if entry.path().is_file() {
+                match load_from_file(entry.path().to_str().unwrap()) {
+                    Ok(quotes) => all_quotes.extend(quotes),
+                    Err(e) => return Err(e),
+                }
+            }
         }
+        Ok(all_quotes)
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            format!("Folder not found: {:?}", path),
+        ))
     }
-    
-    Ok(all_quotes)
 }
